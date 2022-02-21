d6eb68c7c45375cc0a38bfdaf38d6caf3fa0a67a
najongjine <najongjin3@hotmail.com>
2022-02-21 20:49:42 +0900

    .

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject thingToFollow;
+    // this things position (camera) should be the same as the car's position
+
+    void LateUpdate()
     {
-        
+        transform.position = thingToFollow.transform.position + new Vector3(0, 0, -10);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
